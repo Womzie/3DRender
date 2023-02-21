c4b765444d8f5de1da5fb21c68cea1e45bf2ae01
Anders Stall <andersstall@lclark.edu>
2023-02-21 11:19:30 -0800

    maybe fix XYZ writer

--- stbTest.c
+++ stbTest.c
@@ -14,27 +14,27 @@ const size_t NUM_PIXELS_TO_PRINT = 25U;
 int writeJPG(FILE *fptr, unsigned char *data, int w, int h){
     int c = 3;
     int x,y;
+    
 
     fprintf(fptr,"%d %d \n", w, h);
-    for (size_t i = 0; i < w * h * c; i+=c) {
-            
-        for(size_t j = 0; j < c; j++){
-            double hex = data[i + j];
-            hex /= 255;
-            
-            fprintf(fptr, "%lf %s", hex, ((j + 1) % c) ? "" : "\n");  
-        }
-        }
+
+    for (size_t i = 0; i < w * h * c; i+=1) {
+        double hex = data[i];
+        hex /= 255;
         
-        for(x = 0; x < w; x++){
-        for(y = 0; y < h; y++){
+        fprintf(fptr, "%lf %s", hex, ((i + 1) % c) ? "" : "\n");  
+    }
+       
+    for(y = 0; y < w; y++){
+        for(x = 0; x < h; x++){
             fprintf(fptr,"%d %d\n", x, y);
             }
-        }   
-
+    }    
+        
+    
     fclose(fptr);
 
-    return 0;
+    return 1;
 }
 
 int writePNG(FILE *fptr,unsigned char *data, int w, int h){
@@ -42,8 +42,8 @@ int writePNG(FILE *fptr,unsigned char *data, int w, int h){
     int x,y;
     
     fprintf(fptr,"%d %d \n", w, h);
+
     for (size_t i = 0; i < w * h * c; i+=c) {
-            
         if(data[i + c -1] != 0){
             for(size_t j = 0; j < c; j++){
                 if(j != c-1){
@@ -53,18 +53,47 @@ int writePNG(FILE *fptr,unsigned char *data, int w, int h){
                 }
                  
             }
+            
         }
-    }
-        
-    for(x = 0; x < w; x++){
-    for(y = 0; y < h; y++){
-        fprintf(fptr,"%d %d\n", x, y);
+        else{
+            i += c;
         }
-    }   
+    }  
+    for(y = 0; y < w; y++){
+        for(x = 0; x < h; x++){
+            fprintf(fptr,"%d %d\n", x, y);
+        }
+    } 
 
     fclose(fptr);
 
     
+    return 1;
+}
+
+int xyzToString(char *xyzName){
+    printf("working");
+    FILE *fptr;
+    int x, y;
+    double tr, tg, tb;
+    scanf("%s", xyzName);
+    fptr = fopen(xyzName, "r");
+    if(fptr == NULL){
+        printf("ERROR!");
+        exit(1);
+    }
+    fscanf(fptr, "%d", &x);
+    fscanf(fptr, "%d", &y);
+
+    for(int i = 0; i < y; i++){
+        for(int j = 0; j < x; j++){
+            fscanf(fptr, "%lf %lf %lf", &tr, &tg, &tb);
+            printf("( %2.1f, %2.1f, %2.1f )\t", tr, tg, tb);
+        }
+        printf("\n");
+    }
+    fclose(fptr);
+
     return 0;
 }
 
@@ -100,5 +129,7 @@ int main(void) {
         }
 
     }
+
+    xyzToString(xyzName);
     return 0;
 }
