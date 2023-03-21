e2291b574d1578100db84a10d9f2db9547dd3f1d
Womzie <lc20-0100@lclark.edu>
2023-03-21 12:02:02 -0700

    broke everything

--- displayNEWscale.c
+++ displayNEWscale.c
@@ -14,7 +14,7 @@
 
 
 int numoo, gsize[5][2], tp[5];
-double bb,bg,br, xpoints[2][810000], ypoints[2][810000],zpoints[2][810000],psize[2] ;
+double bb,bg,br, xpoints[4][81000], ypoints[4][810000],zpoints[4][810000],psize[4] ;
 int height[5], width[5];
 typedef struct {
   double r ;
@@ -178,7 +178,8 @@ void draw(){
   //G_rgb(1,0,0);
   
   //printf("%d %d \n", numoo, tp[0]);
-  for (int k = 0; k < numoo; k++){
+  //for (int k = 0; k < numoo; k++){
+    int k = 0;
     for (int i = 0; i < tp[k]; i++){
 
       if(thing[k][i].alpha > 0){
@@ -210,7 +211,8 @@ void draw(){
     //G_fill_circle(x,y,1);
       }
       
-    }}
+    }
+    //}
 
 }
 
@@ -249,10 +251,13 @@ void find_range(int c, double r[]){
 
 void adjust_points(int c){
   
-  double t1[4][4], s1[4][4], t2[4][4],res[4][4],z[900],sx,sy,r[2];
+  double t1[4][4], s1[4][4], t2[4][4],res[4][4],z[900],sx,sy,r[2], r1[4][4];
   M3d_make_translation(t1, -xpoints[c][tp[c]],-ypoints[c][tp[c]], -10);
-
+  M3d_make_identity(r1);
+  
   find_range(c, r);
+
+  r1[1][1] = -1;
 
   if (r[0]>r[1]){
     sx = sy = sWIDTH/r[0];
@@ -272,6 +277,7 @@ void adjust_points(int c){
   //M3d_make_scaling(s1, 0.01,0.01,1);
 
   M3d_mat_mult_points(xpoints[c],ypoints[c], zpoints[c],t1,xpoints[c],ypoints[c],zpoints[c], tp[c]+1);
+  M3d_mat_mult_points(xpoints[c],ypoints[c], zpoints[c],r1,xpoints[c],ypoints[c],zpoints[c], tp[c]+1);
   M3d_mat_mult_points(xpoints[c],ypoints[c], zpoints[c],s1,xpoints[c],ypoints[c],zpoints[c], tp[c]+1);
   M3d_mat_mult_points(xpoints[c],ypoints[c], zpoints[c],t2,xpoints[c],ypoints[c],zpoints[c], tp[c]+1);
 
@@ -286,25 +292,34 @@ int main(int argc, char **argv) {
   char list[10][100];
   int i;
   double br, bg,bb;
-  i=1;
-  
+
+
 
   if (argc>5){printf("too many objects.\n"); exit(0);}
 
-  while (i < argc){
-    read_file(argv[i],i-1);
+  printf("How many images? ");
+  scanf("%d", &numoo); printf("\n");
+
+  for (int i =1; i<= numoo; i++){
+    char name[100];
+    printf("What is the name of image %d: ", i);
+    scanf("%s", name);
+    read_file(name,i-1);
     adjust_points(i-1);
-    //printf("adjust points, succeeds \n");
-    i++;
+    G_init_graphics (sWIDTH,sHEIGHT);
+    G_rgb(1,1,1);
+    G_clear();
+    draw();
+    G_wait_key();
+
   }
-  numoo = argc-1;
+
   
   // printf("what is the background color?\n");
   // scanf("%lf %lf %lf", &br,&bg,&bb);
 
-
-  G_init_graphics (sWIDTH,sHEIGHT);
-  G_rgb(1,0,0);
+ 
+  G_rgb(1,1,1);
   G_clear();
   draw();
   G_wait_key();
